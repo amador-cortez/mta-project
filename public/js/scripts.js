f069ac1a92fb90ea9581c4901e50da90d0a7a379
Mmaribel207 <mmaribel207@gmail.com>
2024-12-17 18:03:33 -0800

    Buscar, filtrar y ordenar servicios en dashboard

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,55 +2,226 @@ const checkBoxEle=document.getElementsByName('select-service');
 const checkAllServices = document.getElementById("select-all-services");
 const checkedBoxesLabel = document.getElementById('select-all-services-label');
 
+checkAllServices.addEventListener("load", () =>{
+    updateCheckedLabel();
+});
+checkAllServices.addEventListener("change", () =>{
+    updateCheckedLabel();
+});
+
 //const searchBar = document.getElementById("search-url-bar");
 const lists = document.querySelectorAll('#url-lists li');
 const searchIcon = document.querySelector('.fa-magnifying-glass');
 const xIcon = document.querySelector('.fa-xmark');
 
-/*
-function updateSearchDisplay (){
-    const searchTerm = searchBar.value.trim().toLowerCase();
-    xIcon.style.display = searchTerm ? 'block':'none';
-    searchIcon.style.display = searchTerm ? 'none': 'block';
 
-    lists.forEach(item =>{
-        const service = item.textContent.toLowerCase();
-        item.style.display = service.includes(searchTerm) ? 'block':'none';});
+//Search
+
+const search = () => {
+    const searchBar = document.getElementById("search-url-bar").value.toUpperCase();
+    const serviceListName = document.getElementById("service-list");
+    const services = document.querySelectorAll('.card-service');
+    const sname = serviceListName.getElementsByTagName("h3");
+    let sum = 0;
+    for(let i = 0; i< sname.length; i++){
+        let match = services[i].getElementsByTagName('h3')[0];
+        if(match){
+            let textValue = match.textContent || match.innerHTML;
+
+            if(textValue.toUpperCase().indexOf(searchBar) > -1){
+                services[i].style.display = "";
+            }else {
+                services[i].style.display = "none";
+                sum+=1;
+                
+            }
+        }
+    }   
+}
+
+//ORDER
+const orderBy = () =>{
+    const order = document.getElementById("order-by").value;
+    
+    switch(order){
+        case "active-first":
+            console.log("Activo");
+            orderLabel("Activo");
+            break;
+        case "inactive-first":
+            console.log("Inactivo");
+            orderLabel("Inactivo");
+            break;
+        case "a-z":
+            console.log("A - Z");
+            orderType("A - Z");
+            break;
+        case "z-a":
+            console.log("Z - A");
+            orderType("Z - A");
+            break;
+    }
+
+    
+}
+
+function orderLabel (order) {
+    const serviceListName = document.getElementById("service-list");
+    const services = document.querySelectorAll('.card-service');
+    const slabel = serviceListName.getElementsByTagName("label");
+
+    let newOrderedServices = [];
+    let temp = [];
+
+    console.log(`${order}`+2);
+    for(let i = 0; i< slabel.length; i++){
+        let match = services[i].getElementsByTagName('label')[0];
+        console.log(`Match: ${match}`)
+        if(match){
+            let textValue = match.textContent || match.innerHTML;
+            console.log(`${order} order`);
+            if(textValue == order){
+                newOrderedServices.push(services[i]);
+            }else{
+                temp.push(services[i]);
+            }
+        }
+    }  
+    serviceListName.innerHTML ="";
+    for(let i = 0; i< temp.length; i++){
+        newOrderedServices.push(temp[i]); 
+    }
+    console.log("Final New:");
+    console.log(`New order length: ${newOrderedServices.length}`);
+
+    for(let i = 0; i< newOrderedServices.length; i++){
+        serviceListName.innerHTML += "<li class = 'card-service' >"+newOrderedServices[i].innerHTML +"</li>";
+    }
+    console.log("after");
+}
+function orderType (order) {
+    const serviceListName = document.getElementById("service-list");
+    const services = document.querySelectorAll('.card-service');
+
+    const ogOrder = []
+    console.log(`${order}`+2);
+    for(let i = 0; i< services.length; i++){
+        let objTemp = {
+           "serviceInfo": services[i],
+            "name": services[i].getElementsByTagName('h3')[0]
+        }
+        ogOrder.push(objTemp);
+    }
+    /*
+    for(let i=0;i<ogOrder.length;i++){
+        console.log(ogOrder[i].name.innerHTML);
+        console.log(i);
+    }*/
+
+    if(order == "A - Z"){
+        ogOrder.sort((a,b) => a.name.innerHTML.localeCompare(b.name.innerHTML));
+    
+    }else{
+        ogOrder.sort((a,b) => b.name.innerHTML.localeCompare(a.name.innerHTML));
+    }
+
+    serviceListName.innerHTML ="";
+    /*console.log("Final New:");
+    console.log(`New order length: ${ogOrder.length}`);*/
+
+    for(let i = 0; i< ogOrder.length; i++){
+        serviceListName.innerHTML += "<li class = 'card-service' >"+ogOrder[i].serviceInfo.innerHTML  +"</li>";
+    }
+    console.log("after");
+}
+
+//FILTER
+
+
+function openFilters(){
+    const selectBtn = document.querySelector(".select-btn");
+    selectBtn.classList.toggle("open");
+    items = document.querySelectorAll(".item");
+    items.forEach(item => {
+        item.addEventListener("click", () =>{
+            item.classList.toggle("checked");
+    
+            let checked = document.querySelectorAll(".checked"),
+            btnText = document.querySelector(".btn-text"),
+            itemText = item.querySelector(".item-text");
+            if(checked && checked.length >0){
+                btnText.innerText = `${itemText.innerHTML}`;
+                FilterBy(itemText.innerHTML);
+            }
+        })
+    });
+    
+}
+
+const FilterBy = (type) =>{
+    switch(type){
+        case "Activos":
+            filterLabel("Activo");
+            break;
+        case "Inactivos":
+            filterLabel("Inactivo");
+            break;
+        case "Seleccionar Todo":
+            resetServiceList();
+            break;
+    }
+}
+const filterLabel = (filter) => {
+    //const searchBar = document.getElementById("search-url-bar").value.toUpperCase();
+    const serviceListName = document.getElementById("service-list");
+    const services = document.querySelectorAll('.card-service');
+    const slabel = serviceListName.getElementsByTagName("label");
+    let sum = 0;
+
+    console.log(`${filter}`+2);
+    for(let i = 0; i< slabel.length; i++){
+        let match = services[i].getElementsByTagName('label')[0];
+        if(match){
+            let textValue = match.textContent || match.innerHTML;
+
+            if(textValue == filter){
+                services[i].style.display = "";
+            }else {
+                services[i].style.display = "none";
+                sum+=1;
+                
+            }
+        }
+    }   
+}
+const resetServiceList = () => {
+    //const searchBar = document.getElementById("search-url-bar").value.toUpperCase();
+    const serviceListName = document.getElementById("service-list");
+    const services = document.querySelectorAll('.card-service');
+    for(let i = 0; i< services.length; i++){
+        services[i].style.display = "";
+    }
+    
+      
 }
 
 
-searchBar.addEventListener('input', updateSearchDisplay());
-xIcon.addEventListener('click', ()=> {
-    searchBar.value = '';
-    updateSearchDisplay();
-});*/
-
-//DASHBOARD
-
-//search
-let serviceData = "";
-const urlsContainer = document.querySelector(".service-list");
-
-fetch(
-    "https://gist.githubusercontent.com/jemimaabu/564beec0a30dbd7d63a90a153d2bc80b/raw/0b7e25ba0ebee6dbba216cfcfbae72d460a60f26/tutorial-levels"
-  ).then(async (response) => {
-    serviceData = await response.json();
-    serviceData.map((url) => createMonitor(url));
-  });
-
-  const createMonitor = (serviceData) => {
+const createMonitor = (serviceData) => {
+    console.log("enterde create monitor");
     //const {urlName, link, date} = serviceData;
-    const {link} = serviceData;
+    const {link, domainName} = serviceData;
+    //const domainName = getDomainName(link);
+
     const service = document.createElement("LI");
 
-    service.className("card-service");
+    service.className = "card-service";
     service.innerHTML = '<div class=" left"> <div class="rows"> <div class="left"> '+
     '<input type="checkbox" name = "select-service" class="select-checkBox " onchange="updateCheckedLabel()">'+
     '<label  style="margin-right: 50px;">Activo</label> </div> ' +
      '<div class="right">'+
-        '<h3> Nombre servicio #</h3> '+
+        '<h3> '+domainName+' #</h3> '+
         '<div class = "under">'+
-            '<p id ="domain-type" >Dominio</p><p id = "last-date-check">Última comprobación (fecha y hora).</p>'+
+            '<p id = "last-date-check">Última comprobación (fecha y hora).</p>'+
        ' </div> </div> </div> </div> ' + 
     '<div class="right rows">'+
     '<p id = "check-time-frecuency" class="left">Frecuencia de las comprobaciones</p>'+
@@ -58,51 +229,21 @@ fetch(
     '<i class="fa-solid fa-trash fa-2x" onclick="deleteSelectedService()" ></i> </div> ';
 
     urlsContainer.append(service);
+    console.log(urlsContainer.lastChild);
   }
 
-  //searh listener
-const searchBar = document.getElementById("search-url-bar");
 
-let debounceTimer;
-const debounce = (callback, time) =>{
-    window.clearTimeout(debounceTimer);
-    debounceTimer = window.setTimeout(callback, time);
-};
-/*
-searchBar.addEventListener("input",
-    (event) => {
-        const query = event.target.value;
-        debounce() => handleSearchPsts(query),500)
-    }.false
-);*/
-const search = () => {
-    const searchBar = document.getElementById("search-url-bar");
-    const serviceListName = document.getElementById("service-list");
-    const services = document.querySelectorAll('.card-service');
-    const sname = serviceListName.getElementsByTagName("h3");
 
-    for(let i = 0; i< sname.length; i++){
-        let match = services[i].getElementsByTagName('h3')[0];
-       
-        if(match){
-            let textValue = match.textContent || match.innerHTML;
+//DASHBOARD
 
-            if(textValue.toUpperCase().indexOf(searchBar) > -1){
-                services[i].style.display = "";
-            }else {
-                services[i].style.display = none;
-            }
-        }
-    }
 
-}
 function resetSearch(){
     document.getElementById("search-url-bar").value = "";
 }
 
 //cHECKBOXES
 
-function updateCheckedLabel(checkBox){
+function updateCheckedLabel(){
     
     const checkedBoxesLabel = document.getElementById('select-all-services-label');
     const checkAllServices = document.getElementById("select-all-services");
